486d4cdc6e7510e283a167a73b9dcdcff2330ff5
Payam Khaninejad <khaninejad@gmail.com>
2020-09-24 13:45:51 +0330

    first init

---
+++ SalesbyMatch/SalesbyMatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace SalesbyMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int[] ar= { 10 ,20 ,20, 10 ,10, 30 ,50, 10 ,20,50 } ;
+
+
+            Console.Write(sockMerchant(n, ar));
+
+        }
+        static int sockMerchant(int n, int[] ar)
+        {
+            return ar.GroupBy(a => a).Sum(a => a.Count() / 2);
+
+        }
+    }
+}
