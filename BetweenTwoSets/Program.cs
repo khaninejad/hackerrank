b39b004021e5a3681db13cbd10993f43b44a61a3
Payam Khaninejad <khaninejad@gmail.com>
2020-11-17 11:02:44 +0330

    BetweenTwoSets solutions

---
+++ BetweenTwoSets/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace BetweenTwoSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[]{ 2, 3 };
+            int[] b = new[] {2, 4};
+            var num = getTotalX(a, b);
+            Console.WriteLine("Hello World!");
+        }
+        static int getTotalX(int[] a, int[] b)
+        {
+            var totalXs = 0;
+            var maximumA = a.Max(); //Time-complexity O(n)
+            var minimumB = b.Min(); //Time-complexity O(m)
+            var counter = 1;
+            var multipleOfMaxA = maximumA;
+
+            while (multipleOfMaxA <= minimumB)
+            {
+                var factorOfAll = true;
+
+                foreach (var item in a) //Time complexity O(n)
+                {
+                    if (multipleOfMaxA % item != 0)
+                    {
+                        factorOfAll = false;
+                        break;
+                    }
+                }
+
+                if (factorOfAll)
+                {
+                    foreach (var item in b) //Time complexity O(m)
+                    {
+                        if (item % multipleOfMaxA != 0)
+                        {
+                            factorOfAll = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (factorOfAll)
+                    totalXs++;
+
+                counter++;
+                multipleOfMaxA = maximumA * counter; //Here counter is the x factor which contributes to O(x(n+m)) complexity.
+            }
+            return totalXs;
+        }
+
+    }
+}
